3c296ca4046547c8139efac74c2edc2f53924ab3
LUZ ADRIANA GONZALEZ PACHECO <vi06442@innovaccion.mx>
2021-08-09 12:06:38 -0500

    Creacion de vistas de Marcas y modelosModelos completados e implementacion de Contexto (CodeFirst)

---
+++ Proyecto_FunCase_WEBLY/Controllers/ModeloesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_FunCase_WEBLY.Models;
+
+namespace Proyecto_FunCase_WEBLY.Controllers
+{
+    public class ModeloesController : Controller
+    {
+        private FunCaseModelContext db = new FunCaseModelContext();
+
+        // GET: Modeloes
+        public ActionResult Index()
+        {
+            return View(db.Modelos.ToList());
+        }
+
+        // GET: Modeloes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelos.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+
+        // GET: Modeloes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Modeloes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ModeloID,Nombre,Ancho,Alto,Grosor,ImagenReferencia")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Modelos.Add(modelo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(modelo);
+        }
+
+        // GET: Modeloes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelos.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+
+        // POST: Modeloes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ModeloID,Nombre,Ancho,Alto,Grosor,ImagenReferencia")] Modelo modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(modelo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(modelo);
+        }
+
+        // GET: Modeloes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Modelo modelo = db.Modelos.Find(id);
+            if (modelo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelo);
+        }
+
+        // POST: Modeloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Modelo modelo = db.Modelos.Find(id);
+            db.Modelos.Remove(modelo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
